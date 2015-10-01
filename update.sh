bc9f135ce7e1cad434a06641bf2d50ebb188b1b9
Pieter Smith <pieter@boesman.nl>
2015-10-01 18:11:59 +0200

    update: snapshot build results

--- update.sh
+++ update.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 IMG_DIR=output/images
 BOOT_DIR=/media/`whoami`/boot
-#touch /home/smipi1/Projects/elce2015/buildroot/output/build/linux-*/.stamp_configured &&
-#	make &&
-	cp -v ${IMG_DIR}/MLO ${BOOT_DIR} &&
-	cp -v ${IMG_DIR}/u-boot.img ${BOOT_DIR} &&
-	cp -v ${IMG_DIR}/am335x-boneblack.dtb ${BOOT_DIR} &&
-	cp -v ${IMG_DIR}/zImage ${BOOT_DIR} &&
-	cp -v ${IMG_DIR}/uEnv.txt ${BOOT_DIR} &&
+
+hasLocalChanges() {
+	[ -n "`git diff-index --name-only HEAD --`" ] &&
+		return 0 ||
+		return 1
+}
+
+head() {
+	git show-ref HEAD | awk '{print $1}'
+}
+
+releaseName() {
+	if hasLocalChanges; then
+		echo "`head`++"
+	else
+		head
+	fi
+}
+
+copyImagesTo() {
+	cp -v ${IMG_DIR}/MLO ${1} &&
+	cp -v ${IMG_DIR}/u-boot.img ${1} &&
+	cp -v ${IMG_DIR}/am335x-boneblack.dtb ${1} &&
+	cp -v ${IMG_DIR}/zImage ${1} &&
+	cp -v ${IMG_DIR}/uEnv.txt ${1} &&
+	return 0 ||
+	return 1
+}
+
+RELEASE_DIR=../releases/`releaseName`
+
+[ "${1}" = "tclean" ] && make trivia-dirclean all
+
+mkdir -p -v ${RELEASE_DIR} &&
+	touch /home/smipi1/Projects/elce2015/buildroot/output/build/linux-*/.stamp_configured &&
+	make &&
+	copyImagesTo ${RELEASE_DIR} &&
+	copyImagesTo ${BOOT_DIR} &&
 	ls ${BOOT_DIR} -lh &&
 	sha1sum ${BOOT_DIR}/*
